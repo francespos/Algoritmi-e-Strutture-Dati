48d9c0a960acd113ebe351eab92767d2252c96cc
Francesco Esposito <123214645+francespos@users.noreply.github.com>
2024-10-22 20:36:09 +0200

    Add files via upload

---
+++ Esercizi/Online-Judge/Divide-et-impera/957/main.cpp
@@ -0,0 +1,66 @@
+// Manca il calcolo della complessità, ma funziona
+#include <vector>
+#include <iostream>
+
+struct Indices {
+    int p, r;
+};
+
+int getLastYearIndex(const std::vector<int>& years, int firstYearIndex, 
+    int duration)
+{
+    auto yearIndex = firstYearIndex;
+    
+    while (yearIndex < years.size() &&
+        years[yearIndex] < years[firstYearIndex] + duration) 
+    {
+        ++yearIndex;
+    }
+    
+    return yearIndex - 1;
+}
+
+Indices findMaxIndices(const std::vector<int>& years, 
+    const Indices& oldMaxIndices, int duration, int pos) 
+{
+    if (pos < years.size()) {
+        const auto firstYearIndex = pos;
+        const auto lastYearIndex = 
+            getLastYearIndex(years, firstYearIndex, duration);
+        
+        // Non semplifico gli 1 per far capire cosa sto facendo
+        if (lastYearIndex - firstYearIndex + 1 > 
+            oldMaxIndices.r - oldMaxIndices.p + 1) 
+        {
+            Indices indices = {firstYearIndex, lastYearIndex};
+            return findMaxIndices(years, indices, duration, pos + 1);
+        }
+        
+        return findMaxIndices(years, oldMaxIndices, duration, pos + 1);
+    }
+    
+    return oldMaxIndices;
+} 
+
+Indices findMaxIndices(const std::vector<int>& years, int duration) {
+    return findMaxIndices(years, {0, 0}, duration, 0);
+}
+
+int main() {
+    int duration;
+    std::cin >> duration;
+    
+    int numYears;
+    std::cin >> numYears;
+    
+    std::vector<int> years(numYears);
+    
+    for (int i =  0; i < numYears; ++i) {
+        std::cin >> years[i];
+    }
+    
+    const auto maxIndices = findMaxIndices(years, duration);
+    
+    std::cout << maxIndices.r - maxIndices.p + 1 << " " << years[maxIndices.p]
+        << " " << years[maxIndices.r] << "\n";
+}
---
+++ Esercizi/Personali/max.cpp
@@ -0,0 +1,26 @@
+// Trovare il massimo di una sequenza di numeri positivi in maniera ricorsiva.
+#include <iostream>
+#include <vector>
+
+int max(const std::vector<int>& v, int oldMax, int pos) {
+    if (pos < v.size()) {
+        int value = v[pos];
+        
+        if (value > oldMax) {
+            return max(v, value, pos + 1);
+        }
+        
+        return max(v, oldMax, pos + 1);
+    }
+    
+    return oldMax;
+}
+
+int max(const std::vector<int>& v) {
+    return max(v, 0, 0);
+}
+
+int main() {
+    std::vector<int> v{5, 8, 3, 15, 9, 7};
+    std::cout << "Max = " << max(v) << "\n";
+}
